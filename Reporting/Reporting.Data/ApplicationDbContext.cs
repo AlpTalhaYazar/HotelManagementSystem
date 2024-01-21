676c3ca31359abde15d7a8ba319d8fb26c346d00
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-01-22 00:48:00 +0300

    feat: Add initial database context and migration for reporting data
    Introduced a new ApplicationDbContext class in Reporting.Data, defining the initial entities and their relationships for the reporting module. Additionally, added the first Entity Framework migration to set up the database schema. This marks the groundwork for database operations in the reporting functionality.

---
+++ Reporting/Reporting.Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.Data.Model;
+
+namespace Reporting.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Report> Reports { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<ContactInfo> ContactInfos { get; set; }
+}
---
+++ Reporting/Reporting.Data/Migrations/20240121211935_WholeModelsCreated.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reporting.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class WholeModelsCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactInfos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    HotelId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    Stars = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactInfos");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
