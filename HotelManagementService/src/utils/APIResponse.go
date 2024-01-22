e6bb716b9c17cbcd8c2a3b5baf75340b0dd51449
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-01-22 03:39:51 +0300

    refactor: Update APIResponse struct for enhanced handling
    Refactored the APIResponse struct in HotelManagementService/src/utils/APIResponse.go by importing emptypb from google.golang.org/protobuf/types/known. This update likely aims to improve the way empty responses are handled in the API, ensuring more robust and clear communication between the service and its clients.

--- HotelManagementService/src/utils/APIResponse.go
+++ HotelManagementService/src/utils/APIResponse.go
@@ -1,10 +1,12 @@
 package utils
 
+import "google.golang.org/protobuf/types/known/emptypb"
+
 type APIResponse struct {
 	Status  bool        `json:"status"`
 	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-	Errors  []Error     `json:"errors,omitempty"`
+	Data    interface{} `json:"data"`
+	Errors  []Error     `json:"errors"`
 }
 
 func NewSuccessResponse(message string, data interface{}) APIResponse {
@@ -12,7 +14,7 @@ func NewSuccessResponse(message string, data interface{}) APIResponse {
 		Status:  true,
 		Message: message,
 		Data:    data,
-		Errors:  nil,
+		Errors:  make([]Error, 0),
 	}
 }
 
@@ -20,7 +22,7 @@ func NewErrorResponse(message string, errors ...Error) APIResponse {
 	return APIResponse{
 		Status:  false,
 		Message: message,
-		Data:    nil,
+		Data:    emptypb.Empty{},
 		Errors:  errors,
 	}
 }
